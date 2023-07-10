d154f16fc92f3e62c590dc34fc237bba11506022
Aleksandra Ovchinnikova <nsanjka@gmail.com>
2023-07-09 18:17:47 -0700

    LITE-27884 Merge install and update extension in one command

--- connect/eaas/core/deployment/utils.py
+++ connect/eaas/core/deployment/utils.py
@@ -1,10 +1,12 @@
 import os
 import tempfile
+import time
 from pathlib import Path
 
 import toml
 from yaml import safe_load, scanner
 
+from connect.client import ClientError
 from connect.eaas.core.deployment.helpers import (
     DEFAULT_CLONE_DIR,
     GitException,
@@ -17,17 +19,25 @@ class DeploymentError(Exception):
     pass
 
 
-def extract_arguments(url, is_install=False):  # noqa: CCR001
+def extract_arguments(url, tag):  # noqa: CCR001
     with tempfile.TemporaryDirectory() as temp_path:
         try:
-            clone_repo(temp_path, url)
+            clone_repo(temp_path, url, tag)
         except GitException as ge:
             raise DeploymentError(ge)
 
         repo_path = Path(os.path.join(temp_path, DEFAULT_CLONE_DIR))
-        file_path = os.path.join(repo_path, '.connect-deployment.yaml')
+        file_path = os.path.join(repo_path, 'connect-deployment.yaml')
         if not os.path.exists(file_path):
-            file_path = os.path.join(repo_path, '.connect-deployment.yml')
+            file_path = os.path.join(repo_path, 'connect-deployment.yml')
+        if not os.path.exists(file_path):
+            raise DeploymentError(
+                (
+                    'Deployment file is not found. Please, check, that file with name '
+                    'connect-deployment.yaml or connect-deployment.yml exists and '
+                    'is located in root project directory.'
+                ),
+            )
         try:
             with open(file_path, 'r') as file_data:
                 arguments = safe_load(file_data)
@@ -38,10 +48,10 @@ def extract_arguments(url, is_install=False):  # noqa: CCR001
                 apps = content['tool']['poetry']['plugins']['connect.eaas.ext']
                 arguments['type'] = 'transformations' if 'tfnapp' in apps else 'multiaccount'
 
-            if arguments.get('icon') and is_install:
+            if arguments.get('icon'):
                 arguments['icon'] = open(os.path.join(repo_path, arguments.get('icon')), 'rb')
 
-            if arguments.get('overview') and is_install:
+            if arguments.get('overview'):
                 if os.path.exists(os.path.join(repo_path, arguments['overview'])):
                     with open(os.path.join(repo_path, arguments['overview']), 'r') as overview:
                         arguments['overview'] = overview.read()
@@ -49,8 +59,6 @@ def extract_arguments(url, is_install=False):  # noqa: CCR001
                     arguments['overview'] = None
 
             return arguments
-        except FileNotFoundError as fe:
-            raise DeploymentError(f'No file: {fe}.')
         except OSError as e:
             raise DeploymentError(f'Error opening file: {e}')
         except scanner.ScannerError as se:
@@ -94,19 +102,107 @@ def process_variables(arguments, env_api):
             )
 
 
-def get_git_data(repo, tag, default_tag):
+def get_git_data(repo, tag):
     try:
         tags = list_tags(repo)
     except GitException as ge:
         raise DeploymentError(f'Cannot retrieve git repository {repo} tags info: {ge}.')
 
-    if tag is None or tag not in tags:
-        tag = list(tags.keys())[0] if default_tag is None else str(default_tag)
-    if tag not in tags:
-        raise DeploymentError(f'Invalid tag: {tag}.')
+    if tag is None:
+        tag = list(tags.keys())[0]
+    elif tag not in tags:
+        raise DeploymentError(
+            f'Tag {tag} doesn\'t exist, please, select one of: {", ".join(tags.keys())}.',
+        )
 
     return {
         'tag': tag,
         'commit': tags[tag],
         'url': repo,
     }
+
+
+def get_category(client, arguments, log):
+    category_id = None
+    if arguments.get('category'):
+        log(f'Looking for a category {arguments["category"]}.')
+        try:
+            category = client.dictionary['extensions'].categories.filter(
+                name=arguments.get('category'),
+            ).first()
+            if category:
+                category_id = {'id': category['id']}
+            log(f'Category {arguments["category"]} was{"" if category else " not"} found.')
+        except ClientError as ce:
+            log(f'Error during looking up category: {ce}. Skip it.')
+
+    return category_id
+
+
+def create_extension(client, arguments, log):
+    payload = {
+        'type': arguments['type'],
+        'name': arguments['name'],
+        'package_id': arguments['package_id'],
+        'category': get_category(client, arguments, log),
+        'short_description': arguments.get('description'),
+        'overview': arguments.get('overview'),
+        'website': arguments.get('website'),
+    }
+
+    try:
+        log(f'Creating extension {arguments["package_id"]}.')
+        extension = client('devops').services.create(payload=payload)
+
+        if arguments.get('icon'):
+            log(f'Setting icon for extension {arguments["package_id"]} instance.')
+            extension = client('devops').services[extension['id']].update(
+                files={'icon': arguments.get('icon')},
+            )
+
+        return extension
+    except ClientError as ce:
+        log(f'Error creating extension: {ce}.')
+        return None
+
+
+def update_extension(client, extension, arguments, log):
+    payload = {
+        'category': get_category(client, arguments, log),
+        'short_description': arguments.get('description'),
+        'overview': arguments.get('overview'),
+        'website': arguments.get('website'),
+    }
+
+    try:
+        log(f'Updating extension {arguments["package_id"]}.')
+        extension = client('devops').services[extension['id']].update(payload=payload)
+        return extension
+    except ClientError as ce:
+        log(f'Error updating extension: {ce}.')
+        return None
+
+
+def stop_environment(environment, env_api, log):
+    if environment['status'] != 'running':
+        return environment
+
+    try:
+        log(f'{environment["type"]} environment is running. Ready to stop it.')
+        env_api.action('stop').post()
+
+        elapsed = 0
+        while elapsed < 5:
+            environment = env_api.get()
+            if environment['status'] not in ['stopped', 'errored']:
+                log(f'Environment is not stopped: wait 1s more, elapsed {elapsed}s')
+                time.sleep(1)
+                elapsed += 1
+                continue
+            break
+        else:
+            log('Environment hasn\'t stopped in maximum wait time, please run deploy again.')
+    except ClientError as ce:
+        log(f'Error stopping {environment["type"]} environment: {ce}')
+
+    return environment
---
+++ tests/connect/eaas/core/deployment/test_extension.py
@@ -0,0 +1,360 @@
+import logging
+
+from connect.client import ClientError, ConnectClient
+from connect.eaas.core.deployment.extension import deploy_extension
+from connect.eaas.core.deployment.utils import DeploymentError
+
+
+CLIENT = ConnectClient(
+    'ApiKey XXX:YYY',
+    endpoint='https://localhost/public/v1',
+)
+LOGGER = logging.getLogger('test')
+
+
+def test_deploy_extension_ok_install(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={
+            'package_id': 'p.id',
+            'env': 'test',
+            'type': 'multiaccount',
+            'var': {},
+        },
+    )
+    responses.add(
+        'GET',
+        'https://localhost/public/v1/devops/services?eq(package_id,p.id)&limit=1&offset=0',
+        json=[],
+        status=200,
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.create_extension',
+        return_value={
+            'id': 'SRV-001',
+            'environments': {
+                'test': {'id': 'ENV-001', 'status': 'running', 'runtime': 'cloud'},
+            },
+        },
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.stop_environment',
+        return_value={
+            'id': 'ENV-001',
+            'type': 'test',
+            'runtime': 'cloud',
+            'status': 'stopped',
+        },
+    )
+    mocker.patch('connect.eaas.core.deployment.extension.process_variables')
+    responses.add(
+        'PUT',
+        'https://localhost/public/v1/devops/services/SRV-001/environments/ENV-001',
+        json={},
+        status=201,
+    )
+    responses.add(
+        'POST',
+        'https://localhost/public/v1/devops/services/SRV-001/environments/ENV-001/start',
+        json={},
+        status=201,
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Extension with package_id p.id does not exist, creating it' in caplog.text
+    assert 'Extension with package_id p.id successfully deployed' in caplog.text
+
+
+def test_deploy_extension_ok_update(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={
+            'package_id': 'p.id',
+            'env': 'test',
+            'type': 'multiaccount',
+            'name': 'Extension',
+            'var': {},
+        },
+    )
+    responses.add(
+        'GET',
+        'https://localhost/public/v1/devops/services?eq(package_id,p.id)&limit=1&offset=0',
+        json=[
+            {
+                'id': 'SRV-001',
+                'environments': {
+                    'test': {'id': 'ENV-001', 'status': 'stopped', 'runtime': 'local'},
+                },
+            },
+        ],
+        status=200,
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.update_extension',
+        return_value={
+            'id': 'SRV-001',
+            'environments': {
+                'test': {'id': 'ENV-001', 'status': 'stopped', 'runtime': 'local'},
+            },
+        },
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.stop_environment',
+        return_value={
+            'id': 'ENV-001',
+            'type': 'test',
+            'runtime': 'local',
+            'status': 'stopped',
+        },
+    )
+    mocker.patch('connect.eaas.core.deployment.extension.process_variables')
+    responses.add(
+        'PUT',
+        'https://localhost/public/v1/devops/services/SRV-001/environments/ENV-001',
+        json={},
+        status=201,
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Extension with package_id p.id does exist, updating it' in caplog.text
+    assert 'Extension with package_id p.id successfully deployed' in caplog.text
+
+
+def test_deploy_extension_extracting_error(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        side_effect=DeploymentError('Error retrieving git date'),
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Error retrieving git date' in caplog.text
+    assert 'Extension with package_id p.id successfully deployed' not in caplog.text
+
+
+def test_deploy_extension_no_package_id(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={},
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'No required package_id found' in caplog.text
+    assert 'Extension with package_id p.id successfully deployed' not in caplog.text
+
+
+def test_deploy_extension_filtering_error(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={
+            'package_id': 'p.id',
+            'env': 'test',
+            'type': 'multiaccount',
+            'name': 'Extension',
+            'var': {},
+        },
+    )
+    responses.add(
+        'GET',
+        'https://localhost/public/v1/devops/services?eq(package_id,p.id)&limit=1&offset=0',
+        json={},
+        status=400,
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Error getting extension: 400 Bad Request' in caplog.text
+    assert 'Extension with package_id p.id successfully deployed' not in caplog.text
+
+
+def test_deploy_extension_error_processing_env(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={
+            'package_id': 'p.id',
+            'env': 'test',
+            'type': 'multiaccount',
+            'name': 'Extension',
+            'var': {},
+        },
+    )
+    responses.add(
+        'GET',
+        'https://localhost/public/v1/devops/services?eq(package_id,p.id)&limit=1&offset=0',
+        json=[],
+        status=200,
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.create_extension',
+        return_value={
+            'id': 'SRV-001',
+            'environments': {
+                'test': {'id': 'ENV-001', 'status': 'running', 'runtime': 'cloud'},
+            },
+        },
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.stop_environment',
+        return_value={
+            'id': 'ENV-001',
+            'type': 'test',
+            'runtime': 'cloud',
+            'status': 'stopped',
+        },
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.process_variables',
+        side_effect=ClientError('Ooops'),
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Error processing environment: Ooops' in caplog.text
+    assert 'Extension with package_id p.id successfully deployed' not in caplog.text
+
+
+def test_deploy_extension_env_not_stopped(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={
+            'package_id': 'p.id',
+            'env': 'test',
+            'type': 'multiaccount',
+            'name': 'Extension',
+            'var': {},
+        },
+    )
+    responses.add(
+        'GET',
+        'https://localhost/public/v1/devops/services?eq(package_id,p.id)&limit=1&offset=0',
+        json=[],
+        status=200,
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.create_extension',
+        return_value={
+            'id': 'SRV-001',
+            'environments': {
+                'test': {'id': 'ENV-001', 'status': 'running', 'runtime': 'cloud'},
+            },
+        },
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.stop_environment',
+        return_value={
+            'id': 'ENV-001',
+            'type': 'test',
+            'runtime': 'cloud',
+            'status': 'running',
+        },
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Extension with package_id p.id successfully deployed' not in caplog.text
+
+
+def test_deploy_extension_error_creating(caplog, responses, mocker):
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.get_git_data',
+        return_value={'tag': '1.2', 'commit': 'commit_hash', 'url': 'https://github.com/dummy'},
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.extract_arguments',
+        return_value={
+            'package_id': 'p.id',
+            'env': 'test',
+            'type': 'multiaccount',
+            'name': 'Extension',
+            'var': {},
+        },
+    )
+    responses.add(
+        'GET',
+        'https://localhost/public/v1/devops/services?eq(package_id,p.id)&limit=1&offset=0',
+        json=[],
+        status=200,
+    )
+    mocker.patch(
+        'connect.eaas.core.deployment.extension.create_extension',
+        return_value=None,
+    )
+
+    with caplog.at_level(logging.DEBUG):
+        deploy_extension(
+            'https://github.com/dummy',
+            CLIENT,
+            LOGGER.info,
+            '1.2',
+        )
+
+    assert 'Extension with package_id p.id successfully deployed' not in caplog.text
